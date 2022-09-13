4c314b5c9f694f97fa8ea02551166c772320c07e
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-09-13 15:29:40 +0700

    No changes

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,4 +1,10 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output
+} from '@angular/core';
 import {Todo} from "../../Todo";
 
 @Component({
